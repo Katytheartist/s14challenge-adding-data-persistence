dd7e3c32e5ecd33eab6775227aee5f375200316c
Katy Kane <katytheartist@gmail.com>
2024-05-22 21:14:07 -0500

    wip

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
-// build your server here and require it from index.js
+const express = require('express')
+const resourceRouter = require('./resource/router')
+//const projectRouter = require('./project/router')
+//const taskRouter = require('./task/router')
+
+const server = express()
+
+server.use(express.json())
+
+server.use('/api/resource', resourceRouter)
+//server.use('/api/project', projectRouter)
+//server.use('/api/task', taskRouter)
+
+module.exports = server
