7958304a32540798ecd34843024b6fece959b568
Katy Kane <katytheartist@gmail.com>
2024-05-25 13:13:36 -0500

    terminaste!!

--- api/task/model.js
+++ api/task/model.js
@@ -5,21 +5,27 @@
 const db = require('../../data/dbConfig')
 
 async function getAll(){
-    return Promise.resolve('tasks comin')
-    // const tasks = await db('tasks as t')
-        // .select(
-        //     'task_id',
-        //     'task_name',
-        //     'task_description',
-        //     'task_completed'
-        // )
+    //return Promise.resolve('tasks comin')
+     const tasks = await db('tasks as t')
+     .leftJoin('projects as p', 't.project_id', 'p.project_id')
+         .select(
+             't.task_id',
+             't.task_notes',
+             't.task_description',
+             't.task_completed',
+             'p.project_name',
+             'p.project_description'
+         )
+         .orderBy('t.task_id')
 
-        // return tasks.map(task=>({
-        //     task_id: task.task_id,
-        //     task_name: task.task_name,
-        //     task_description: task.task_description,
-        //     task_completed: !!task.task_completed
-        // }))
+         return tasks.map(row=>({
+            task_id: row.task_id,
+            task_notes: row.task_notes,
+            task_description: row.task_description,
+            task_completed: !!row.task_completed,
+            project_name: row.project_name,
+            project_description: row.project_description,
+         }))
 }
 
 async function create(taskData){
@@ -28,7 +34,7 @@ async function create(taskData){
         task_completed: taskData.task_completed ? 1 : 0
     }
     const [newTaskId] = await db('tasks').insert(dataToInsert)
-    const newTask = await db('projects').where({project_id: newTaskId}).first()
+    const newTask = await db('tasks').where({task_id: newTaskId}).first()
 
     if(!newTask){
         throw new Error('new task not found')
@@ -36,9 +42,10 @@ async function create(taskData){
 
     return {
         task_id: newTask.task_id,
-        task_name: newTask.task_name,
+        task_notes: newTask.task_notes,
         task_description: newTask.task_description,
-        task_completed: !!newTask.task_completed
+        task_completed: !!newTask.task_completed,
+        project_id: newTask.project_id,
     }
 }
 
--- data/seeds/tasks.js
+++ data/seeds/tasks.js
@@ -1,3 +1,26 @@
-exports.seed =  function(knex){
-    
+const tasks = [
+    {task_description: 'rest feet', 
+        task_notes: 'easier said than done',
+        task_completed: false,
+        project_id: 1
+    },
+    {task_description: 'plant seeds', 
+        task_notes: 'chaos gardening, toss em out',
+        task_completed: true,
+        project_id: 4
+    },
+    {task_description: 'finish this challenge', 
+        task_notes: 'type like mad',
+        task_completed: false,
+        project_id: 1
+    },
+    {task_description: 'advertise happy appy', 
+        task_notes: 'market the idea before creating, the american way',
+        task_completed: false,
+        project_id: 3
+    },
+]
+
+exports.seed =  async function(knex){
+    await knex('tasks').insert(tasks)
 }
